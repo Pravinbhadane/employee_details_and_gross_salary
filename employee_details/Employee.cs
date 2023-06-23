ce76f9014fd987f6f183679db570dc20442fa298
Pravin Bhadane <pravinbhadane60@gmail.com>
2023-06-23 15:59:11 +0530

    Add project files.

---
+++ employee_details/Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_details
+{
+    public  interface IPrint
+    {
+        string PrintDetails();
+    }
+
+    public class Employee : IPrint
+    {
+        protected int id;
+        protected string name;
+        protected int salary;
+        protected double gross_salary, total_salary;
+        private static int count; 
+
+        public Employee(string name,int salary)
+        {
+            count++;
+            this.id = count;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public static int GetCount()
+        {
+            return count;
+        }
+
+        public virtual void CalculateSalary()
+        {
+          double  hra = 40.00 / 100 * salary;
+          double ta = 20.00 / 100 * salary;
+          double  da = 10.00 / 100 * salary;
+          double pf = 12.00 / 100 * salary;
+
+            total_salary = salary + hra  + ta + da;
+           gross_salary =total_salary - (pf);
+        }
+
+
+        public string PrintDetails()
+        {
+            return $"Employee ID = {id}\n Name = {name}\n Total Salary{total_salary}\n Gross Salary = {gross_salary}";
+        }
+    }
+
+    public class SalesManager :Employee, IPrint
+    {
+        private double comm;
+        public SalesManager(string name, int salary, double comm):base (name,salary)
+        {
+            this.comm = comm;
+        }
+
+        public override void CalculateSalary()
+        {
+            double hra = 40.00 / 100 * salary;
+            double ta = 20.00 / 100 * salary;
+            double da = 10.00 / 100 * salary;
+            double pf = 12.00 / 100 * salary;
+
+            total_salary = salary + hra + ta + da +comm;
+            gross_salary = total_salary - (pf);
+        }
+    
+        public string PrintDetails()
+        {
+            return $"Employee ID = {id}\n Name = {name}\n Total Salary{total_salary}\n Gross Salary = {gross_salary}";
+        }
+    }
+}
---
+++ employee_details/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using employee_details;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Employee emp1 = new Employee("pravin", 50000);
+        emp1.CalculateSalary();
+        Console.WriteLine(emp1.PrintDetails());
+
+        Employee emp2 = new Employee("pawan", 60000);
+        emp2.CalculateSalary();
+        Console.WriteLine(emp2.PrintDetails());
+
+        SalesManager salesManager = new SalesManager("Rohan", 28000, 3500);
+        salesManager.CalculateSalary();
+
+        Console.WriteLine(salesManager.PrintDetails());// ToString() get called implicitly 
+
+
+
+        Employee employee = new Employee("Suraj", 28000);
+        employee.CalculateSalary();
+
+        Console.WriteLine(employee.PrintDetails());
+
+        //Console.WriteLine(emp1.PrintDetails());
+        //Console.WriteLine(emp2.PrintDetails());
+        //Console.WriteLine(Employee.GetCount());
+
+
+        //CLASS CALCULATION
+
+        //Calculation task1 = new Calculation();
+
+        //int result1 = task1.Addition(1, 2);
+        //int result2 = task1.Addition(1, 2,3);
+        //double result3 = task1.Addition(55.78, 22.34);
+
+        //int result4 = task1.Subtraction(1, 2);
+        //int result5 = task1.Subtraction(1, 2 ,3);
+        //double result6 = task1.Subtraction(55.78, 22.34);
+
+        //int res1 = task1.Multiplication(1, 2);
+        //int res2 =  task1.Multiplication(1, 2 , 3);
+        //double res3 = task1.Multiplication(55.78, 22.34);
+
+        //Console.WriteLine($"{result1}\n{result2}\n{result3}\n{result4}\n{result5}\n{result6}\n{res1}\n{res2}\n{res3}\n");
+
+
+
+    }
+}
+
